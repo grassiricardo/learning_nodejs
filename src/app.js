5088797d4e22bb3190ba7597a9978d616b702d8b
grassiricardo <grassiricardorg@gmail.com>
2017-09-23 21:58:18 -0300

    create curd rest

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const get = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API",
         version: "0.0.1"
     });
 });
-app.use('/', route)
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
+app.use('/products', get, create, put, del);
 
 module.exports = app;
