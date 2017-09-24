6abb8ebe7aac3c07a72d0c23592c159ef47423bf
grassiricardo <grassiricardorg@gmail.com>
2017-09-24 09:33:25 -0300

    update and delete

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -64,13 +64,37 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                slug: req.body.slug
+            }
+        }).then(data =>{
+            res.status(200).send({
+                message: 'Produto atualizado com sucesso!'
+            });
+        }).catch(e =>{
+            res.status(400).send({
+                message: 'Falha ao atualizar o produto',
+                data: e
+            });
+        });
 };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Product
+    .findOneAndRemove(req.body.id)
+    .then(data =>{
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });
+    }).catch(e =>{
+        res.status(400).send({
+            message: 'Falha ao remover o produto',
+            data: e
+        });
+    });
 };
