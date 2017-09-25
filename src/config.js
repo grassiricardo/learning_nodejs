760450970745c7ca96a8b54e378915e7d4b92dc9
grassiricardo <grassiricardorg@gmail.com>
2017-09-24 23:11:49 -0300

    implements send mail

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://grassi:123@ds064198.mlab.com:64198/node-store-grassi',
-    sendgridKey: 'SUA CHAVE',
+    sendgridKey: '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',
     containerConnectionString: 'SUA CONNECTION STRING'
 }
